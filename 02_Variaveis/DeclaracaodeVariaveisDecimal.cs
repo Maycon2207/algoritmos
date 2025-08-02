a625818c804d4aef75a194c65f67c9f3c8105a9d
Maycon Douglas <maycondouglas9292senac@gmail.com>
2025-08-01 22:06:25 -0300

    Declaracao de Variaveis Decimal

---
+++ 02_Variaveis/DeclaracaodeVariaveisDecimal.cs
@@ -0,0 +1,10 @@
+using System;
+class DeclaracaoVariaveisDecimal
+	{
+		static void Main(string[] args)
+		{
+			decimal mymoneyCorrect = 300.0m;
+			console.Writeline(myMOneyCorrect);
+			
+		}
+	}	
