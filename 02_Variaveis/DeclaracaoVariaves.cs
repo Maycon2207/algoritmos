50aa080020822f8d8ba90f3c117b25c0af29f271
Maycon Douglas <maycondouglas9292senac@gmail.com>
2025-08-01 21:46:42 -0300

    Declaraçao de variaves

---
+++ 02_Variaveis/DeclaracaoVariaves.cs
@@ -0,0 +1,26 @@
+using System;
+
+class DeclaracaoVariaveis
+{
+    static void Main()
+    {
+        // Declaração da variável
+        int a;
+
+        // Inicialização da variável
+        a = 13;
+        Console.WriteLine(a);
+
+        // Reatribuição de valor na variável
+        a = 53;
+        Console.WriteLine(a);
+
+        // Declarando e inicializando outra variável
+        int b = 925;
+        Console.WriteLine(b);
+		
+		int y = 10, x = 20;
+		int z = y + x;
+		Console.WriteLine(z);
+    }
+}
