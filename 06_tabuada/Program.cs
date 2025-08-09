e8aedc7fdab7ea7c6043b0b8068a73eaf383c05f
Maycon Douglas <maycondouglas9292senac@gmail.com>
2025-08-08 22:20:03 -0300

    calculadora

---
+++ 06_tabuada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06_tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Digite um número inteiro: ");
+            numero = int.Parse(Console.ReadLine());
+
+            // Loop de 1 até 10 para mostrar a tabuada
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+            }
+
+         
+        }
+    }
+}
