1d3692965c36910a4238ec21b6f1ac1def8400f5
Maycon Douglas <maycondouglas9292senac@gmail.com>
2025-08-28 20:40:24 -0300

    ajuste para por todos os meses

--- 09_Calendario/Program.cs
+++ 09_Calendario/Program.cs
@@ -12,8 +12,28 @@ namespace _09_Calendario
             Console.Write("Digite o ano:");
             int ano = int.Parse(Console.ReadLine());
 
-            int diasDoMes = DateTime.DaysInMonth(ano, mes);
+            
+            ImprimeCalendario(mes, ano);
 
+            // for (int mes = 1; mes <== 12; mes++)  isso é para ele ficar fazendo um loop e imprimindo todos os meses do ano
+            Console.WriteLine("\n\nDeseja ver o ano inteiro? (S/N): ");
+            string opcao = Console.ReadLine().ToUpper();
+
+            if (opcao == "S")
+            {
+                for (int m = 1; m <= 12; m++)
+                {
+                    ImprimeCalendario(m, ano);
+                    Console.WriteLine(new string('-', 50));
+                }
+                Console.ReadKey();
+            }
+           }
+
+        
+        public static void ImprimeCalendario(int mes, int ano)
+        {
+            int diasDoMes = DateTime.DaysInMonth(ano, mes);
             DateTime primeiroDiaMes = new DateTime(ano, mes, 1);
             int diaSemanaInicio = (int)primeiroDiaMes.DayOfWeek;
 
@@ -36,9 +56,7 @@ namespace _09_Calendario
                 }
             }
 
-            Console.WriteLine($"\nCalendário de " +
-            $"{primeiroDiaMes.ToString("MMMM")} de {ano}");
-
+            Console.WriteLine($"\nCalendário de {primeiroDiaMes.ToString("MMMM")} de {ano}");
             Console.WriteLine("DOM\tSEG\tTER\tQUA\tQUI\tSEX\tSAB");
 
             int[] diasFeriados = RetornaFeriados(mes, ano);
@@ -55,12 +73,10 @@ namespace _09_Calendario
                     }
                     else
                     {
-                        // Domingo em vermelho
                         if (diaSemana == 0)
                         {
                             Console.ForegroundColor = ConsoleColor.Red;
                         }
-                        // Se for feriado, pinta em amarelo
                         else if (Array.Exists(diasFeriados, f => f == valorDia))
                         {
                             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -76,40 +92,36 @@ namespace _09_Calendario
                 }
                 Console.WriteLine();
             }
+
             Console.WriteLine("\nFeriados: ");
-            for (int i = 0; i < diasFeriados.Length; i++)
+            foreach (int f in diasFeriados)
             {
-                if (diasFeriados[i] > 0)
+                if (f > 0)
                 {
-                    Console.Write(diasFeriados[i] + " ");
+                    Console.Write(f + " ");
                 }
             }
-
-            Console.ReadKey();
+            Console.WriteLine("\n");
         }
 
+        
         public static int[] RetornaFeriados(int mes, int ano)
         {
             int[] feriados = new int[10];
             int indice = 0;
 
             if (mes == 1) feriados[indice++] = 1;
-
             else if (mes == 4) feriados[indice++] = 21;
-
             else if (mes == 5) feriados[indice++] = 1;
-
             else if (mes == 9) feriados[indice++] = 7;
-
             else if (mes == 10) feriados[indice++] = 12;
-
             else if (mes == 11)
             {
                 feriados[indice++] = 2;
                 feriados[indice++] = 15;
                 feriados[indice++] = 20;
             }
-            else if (mes == 12) feriados[indice++] = 25; // Natal
+            else if (mes == 12) feriados[indice++] = 25;
 
             DateTime pascoa = DomingoDePascoa(ano);
             DateTime carnaval = pascoa.AddDays(-47);
@@ -123,7 +135,6 @@ namespace _09_Calendario
 
             return feriados;
         }
-
 
         public static DateTime DomingoDePascoa(int ano)
         {
@@ -147,32 +158,28 @@ namespace _09_Calendario
             if (d + g > 9)
             {
                 dia = d + g - 9;
-                mes = 4; // Abril
+                mes = 4;
             }
             else
             {
                 dia = d + g + 22;
-                mes = 3; // Março
+                mes = 3;
             }
+            
+  
+                    return new DateTime(ano, mes, dia);
+                
+                
 
-            // Corrigindo a mensagem para exibir as datas corretamente
-            DateTime pascoa = new DateTime(ano, mes, dia);
-            DateTime carnaval = pascoa.AddDays(-47);
-            DateTime sextaSanta = pascoa.AddDays(-2);
-            DateTime corpusChristi = pascoa.AddDays(60);
-            return pascoa;
-            Console.WriteLine($"O Domingo de Páscoa em {pascoa:dd/MM/yyyy}");
-            Console.WriteLine($"A Terça-feira de carnaval ocorre em {carnaval:dd/MM/yyyy}");
-            Console.WriteLine($"A Sexta-feira santa ocorre em {sextaSanta:dd/MM/yyyy}");
-            Console.WriteLine($"Corpus Christi ocorre em {corpusChristi:dd/MM/yyyy}");
 
-        }
+            }
     }
 }
 
-    
 
 
 
-    
+
+
+
 
